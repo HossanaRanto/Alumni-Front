8741df334d3477464ee506d1e7d7b067fc39e07f
HossanaRanto <mahefaniainaranto@gmail.com>
2025-08-22 06:53:32 +0300

    add auth guard if there is no connected user

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,15 +7,23 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { TestComponent } from './test/test.component';
 import { USER_ROUTES } from './user/user.routes';
 import { ADMIN_ROUTES } from './admin/admin.routes';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [LoginGuard] },
   // TODO: Convert user and admin modules to standalone and update these routes
-  { path: '', loadComponent: () => import('./user/features/simple/main/main.component').then(m => m.MainComponent), canMatch: [], children: [
+  { 
+    path: '', 
+    loadComponent: () => import('./user/features/simple/main/main.component').then(m => m.MainComponent), 
+    canMatch: [authGuard], 
+    children: [
     ...USER_ROUTES
   ] },
-  { path: 'admin', loadComponent: () => import('./admin/features/dashboard/dashboard.component').then(m => m.DashboardComponent), canMatch: [AdminGuard], children: [
+  { 
+    path: 'admin', 
+    loadComponent: () => import('./admin/features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+     canMatch: [AdminGuard], children: [
     ...ADMIN_ROUTES
   ] },
   { path: 'test', component: TestComponent },

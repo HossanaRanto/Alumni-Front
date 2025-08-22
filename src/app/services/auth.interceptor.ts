a0c8484c46e99c5efc2ebe06a5404ba6b5698538
HossanaRanto <mahefaniainaranto@gmail.com>
2025-08-22 06:53:15 +0300

    use HttpInterceptorFn instead of a class intercptor

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,75 +1,74 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpInterceptorFn, HttpHandlerFn } from '@angular/common/http';
 import { catchError, Observable, switchMap, tap, throwError } from 'rxjs';
 import jwtDecode from 'jwt-decode';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { TokenModel } from '../Models/Model';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
+const logout = (auth: AuthService, router: Router) =>{
+  auth.Logout()
+  router.navigate(['/login']);
+}
 
-  constructor(private router:Router,private auth:AuthService) {}
+const handleRefreshToken = (auth: AuthService, router: Router, request:HttpRequest<any>,next:HttpHandlerFn) =>{
+    return auth.RefreshToken().pipe(
+      tap(token=>{
+      }),
+      switchMap(data=>{
+        auth.SetToken(data)
+        return next(addTokenHeader(request, data.accessToken))
+      }),
+      catchError((error,res)=>{
+        logout(auth, router)
+        return []
+      })
+    )
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.headers.has('X-Skip-Interceptor')) {
+const addTokenHeader = (request:HttpRequest<any>,token:string) =>{
+    return request.clone({headers:request.headers.set('Authorization',token)})
+}
+
+export const authInterceptor: HttpInterceptorFn = (request, next) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if (request.headers.has('X-Skip-Interceptor')) {
       // Skip the interceptor and just send the request
-      return next.handle(request);
+      return next(request);
     }
     
     // Check if the request has an Authorization header with a Bearer token
-    const authToken = this.auth.GetAccessToken()  
+    const authToken = auth.GetAccessToken()  
     if (authToken!="") {
       // Decode the JWT to check if it's expired
       const tokenPayload:any = jwtDecode(authToken);
       const currentTime = Date.now() / 1000;
       if (tokenPayload.exp < currentTime) {
         // Token is expired, refresh the token
-        const refresh_token=this.auth.GetRefreshToken()
+        const refresh_token= auth.GetRefreshToken()
         if(refresh_token==""){
           const error_refresh=new Error("Refresh token not found")
-          this.logout()
-          return throwError(error_refresh)
+          logout(auth, router)
+          return throwError(() => error_refresh)
         }
         const refreshTokenPayload:any=jwtDecode(refresh_token)
         if(refreshTokenPayload.exp<currentTime){
           const error_refresh=new Error("Refresh token expired")
-          this.logout()
-          return throwError(error_refresh)
+          logout(auth, router)
+          return throwError(() => error_refresh)
         }
         
-        return this.handleRefreshToken(request,next)
+        return handleRefreshToken(auth, router, request, next)
         // return;
       }
       // Add the Authorization header with the token to the request
-      return next.handle(this.addTokenHeader(request,authToken))
+      return next(addTokenHeader(request,authToken))
     }
     else{
-      this.logout()
+      logout(auth, router)
       const error=new Error("No token found")
-      return throwError(error)
+      return throwError(() => error)
     }
-    // return next.handle(request);
-  }
-  logout(){
-    this.auth.Logout()
-    this.router.navigate(['/login']);
-  }
-  handleRefreshToken(request:HttpRequest<any>,next:HttpHandler){
-    return this.auth.RefreshToken().pipe(
-      tap(token=>{
-      }),
-      switchMap(data=>{
-        this.auth.SetToken(data)
-        return next.handle(this.addTokenHeader(request,data.accessToken))
-      }),
-      catchError((error,res)=>{
-        this.logout()
-        return []
-      })
-    )
-  }
-  addTokenHeader(request:HttpRequest<any>,token:string){
-    return request.clone({headers:request.headers.set('Authorization',token)})
-  }
 }
